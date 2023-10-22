b2789ac77d74ba6eb1e2d92d0c17015966eea0d0
samuel moreira <samuel.moreira.tovar93@gmail.com>
2023-10-22 13:36:45 +0100

    Update_Finally

--- django_base/settings.py
+++ django_base/settings.py
@@ -1,5 +1,4 @@
 import os
-import environ
 import dj_database_url
 
 from pathlib import Path
@@ -8,9 +7,6 @@ from dotenv import load_dotenv
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 load_dotenv(os.path.join(BASE_DIR, ".env" ))
-
-env = environ.Env()
-environ.Env.read_env( os.path.join(BASE_DIR, '.env') )
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
